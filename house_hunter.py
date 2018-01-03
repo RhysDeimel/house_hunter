958a459ec316e0fa41967e690cebea0c07d32c91
RhysDeimel <deimelr@hotmail.com>
2018-01-03 22:45:03 +1100

    class properties and commenting out planned code

--- house_hunter.py
+++ house_hunter.py
@@ -24,10 +24,18 @@ suburb_list = []
 
 class HouseType():
     """docstring for ClassName"""
-    def __init__(self):
-        self.num_bedrooms = None
-        self.num_bathrooms = None
-        self.some_other_param = None
+    def __init__(self, locations=[], property_type=[], min_beds=None,
+                 max_beds=None, min_price=None, max_price=None, bathrooms=None,
+                 car_spaces=None):
+
+        self.locations = locations
+        self.property_type = []
+        self.min_beds = min_beds
+        self.max_beds = max_beds
+        self.min_price = min_price
+        self.max_price = max_price
+        self.bathrooms = bathrooms
+        self.car_spaces = car_spaces
         self.listing_results = []  # a list of objects that are successfull matches
 
     def realestate_url(self):
@@ -54,14 +62,14 @@ nine_am = ''  # integer epoch timestamp of wednesday 9am. Needs time logic
 
 # call for Rhys
 
-destination = secrets.rhys_work
+# destination = secrets.rhys_work
 
-result = gmaps.distance_matrix(origin,
-                               destination,
-                               mode='transit',
-                               language='en-AU',
-                               units='metric',
-                               arrival_time=nine_am)
+# result = gmaps.distance_matrix(origin,
+#                                destination,
+#                                mode='transit',
+#                                language='en-AU',
+#                                units='metric',
+#                                arrival_time=nine_am)
 
 # returns:
 # {'destination_addresses': ['secret destination'], 'origin_addresses': ['secret origin'], 'rows': [{'elements': [{'distance': {'text': '5.9 km', 'value': 5918}, 'duration': {'text': '26 mins', 'value': 1568}, 'status': 'OK'}]}], 'status': 'OK'}
@@ -69,14 +77,14 @@ result = gmaps.distance_matrix(origin,
 
 # call for Kristen
 
-destination = secrets.kristen_work
+# destination = secrets.kristen_work
 
-result = gmaps.distance_matrix(origin,
-                               destination,
-                               mode='driving',
-                               language='en-AU',
-                               units='metric',
-                               arrival_time=nine_am)
+# result = gmaps.distance_matrix(origin,
+#                                destination,
+#                                mode='driving',
+#                                language='en-AU',
+#                                units='metric',
+#                                arrival_time=nine_am)
 
 # returns:
 # {'destination_addresses': ['secret destination'], 'origin_addresses': ['secret origin'], 'rows': [{'elements': [{'distance': {'text': '16.1 km', 'value': 16096}, 'duration': {'text': '19 mins', 'value': 1116}, 'status': 'OK'}]}], 'status': 'OK'}
@@ -89,5 +97,5 @@ result = gmaps.distance_matrix(origin,
 # https://www.realestate.com.au/rent/property-townhouse-house-with-studio-between-0-1100-in-crows+nest%2c+nsw+2065%3b+wollstonecraft%2c+nsw+2065/list-1?numBaths=2&maxBeds=3&source=location-search
 
 
-
-
+if __name__ == '__main__':
+    pass
